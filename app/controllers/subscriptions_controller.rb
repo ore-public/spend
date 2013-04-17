5c6aaf63937db26a67d7569ea3b89f447709c84f
Masaya Konishi <konishi@office-ore.net>
2013-04-17 23:26:43 +0900

    作品の購読の承認・否認ができるように

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,4 +5,22 @@ class SubscriptionsController < ApplicationController
     @subscriptions = Collection.find(params[:collection_id]).subscriptions
 
   end
+
+  def approve
+    subscription = Collection.find(params[:collection_id]).subscriptions.find(params[:id])
+    subscription.status = :read
+    subscription.save
+
+    redirect_to collection_subscriptions_path, {notice: I18n.t('collections.subscriptions.message.approve')}
+  end
+
+  def reject
+    subscription = Collection.find(params[:collection_id]).subscriptions.find(params[:id])
+    subscription.status = :reject
+    subscription.save
+
+    redirect_to collection_subscriptions_path, {notice: I18n.t('collections.subscriptions.message.reject')}
+  end
+
+
 end
