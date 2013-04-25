420893a72a0be595a6fad03458b583090bdba6b1
Masaya Konishi <konishi@office-ore.net>
2013-04-25 20:40:00 +0900

    購読していない作品の動画を見ようとすると、権限エラーが出るように

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => t("cancan.error")
+    redirect_to url_for(controller: exception.message, action: :index), :alert => t("cancan.error")
   end
 
 end
--- app/controllers/readable_contents_controller.rb
+++ app/controllers/readable_contents_controller.rb
@@ -6,7 +6,7 @@ class ReadableContentsController < ApplicationController
     if collection.read?(current_user)
       @contents = collection.contents
     else
-
+      raise CanCan::AccessDenied.new("readable_collections", :read, Content)
     end
 
   end
