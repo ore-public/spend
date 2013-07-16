505aaf318c82120021b604b80ff3501c3b9f7b7b
Masaya Konishi <konishi@office-ore.net>
2013-07-16 14:09:48 +0900

    購読情報検索処理を共通化

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,5 +1,6 @@
 class SubscriptionsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :find_subscriptions, only: [:approve, :reject]
 
   def index
     @subscriptions = Collection.find(params[:collection_id]).subscriptions
@@ -7,20 +8,22 @@ class SubscriptionsController < ApplicationController
   end
 
   def approve
-    subscription = Collection.find(params[:collection_id]).subscriptions.find(params[:id])
-    subscription.status = :read
-    subscription.save
+    @subscription.status = :read
+    @subscription.save
 
     redirect_to collection_subscriptions_path, {notice: I18n.t('collections.subscriptions.message.approve')}
   end
 
   def reject
-    subscription = Collection.find(params[:collection_id]).subscriptions.find(params[:id])
-    subscription.status = :reject
-    subscription.save
+    @subscription.status = :reject
+    @subscription.save
 
     redirect_to collection_subscriptions_path, {notice: I18n.t('collections.subscriptions.message.reject')}
   end
 
+  private
+  def find_subscriptions
+    @subscription = Collection.find(params[:collection_id]).subscriptions.find(params[:id])
 
+  end
 end
