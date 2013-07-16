847d18c03e2a9873e3607a3a6d0181f4220f8d70
Masaya Konishi <konishi@office-ore.net>
2013-07-16 18:18:09 +0900

    ユーザー登録のテスト作成

--- spec/steps/login.steps.rb
+++ spec/steps/login.steps.rb
@@ -1,17 +1,21 @@
 steps_for :login do
   step "会員登録ページにアクセスする" do
-
+    visit '/users/sign_up'
   end
 
   step "IDとパスワードを入力する" do
-
+    fill_in "Email", with: "test@email.com"
+    fill_in "user_password", with: "test0000"
+    fill_in "Password confirmation", with: "test0000"
   end
 
   step "登録ボタンを押す" do
-
+    click_button "Sign up"
   end
 
   step "ログイン済みでトップページが開かれること" do
-
+    current_path = URI.parse(current_url).path
+    current_path.should == "/"
+    User.first.email.should == "test@email.com"
   end
 end
