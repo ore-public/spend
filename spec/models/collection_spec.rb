103773ff4f5d2c8116032d27e57fe2f9f6f821e9
Masaya Konishi <konishi@office-ore.net>
2013-11-23 11:53:01 +0900

    コレクションのモデルテストを新規作成

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
-describe Subscription do
+describe Collection do
   context "正常系パターンで登録した場合" do
     before do
       u = User.create!(email: 'test@email.com', password: 'test0000')
-      @s = Subscription.new
-      @s.user = u
-      @s.save!
+      @c = Collection.new
+      @c.owner = u
+      @c.authentication = :unnecessary
+      @c.save
     end
 
     it "正常に登録できること" do
-      @s.user.email.should == 'test@email.com'
+      @c.owner.email.should == 'test@email.com'
     end
   end
 end
